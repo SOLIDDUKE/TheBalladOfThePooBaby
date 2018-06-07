eb0de1bc6cc60e2c7d6208a80eea0c8e5545558a
jonathan sands <j.sands3@hotmail.co.uk>
2018-06-07 14:56:15 +0100

    script add for din to test
    just added this for din to check

---
+++ BoxieInteract.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoxieInteract : Interactable {
+
+    public int delay;
+    MeshRenderer meshRenderer;
+    Color originalColor;
+
+    public override void Interact()
+    {
+        base.Interact();
+        // originalColor = GetComponent<MeshRenderer>().material.color;
+        // GetComponent<MeshRenderer>().material.color = Color.red;
+        GetComponent<Animesh>().m_CurrentAnimesh = "Walk";
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(delay);
+        // GetComponent<MeshRenderer>().material.color = originalColor;
+        GetComponent<Animesh>().m_CurrentAnimesh = "Spike";
+    }
+
+}
